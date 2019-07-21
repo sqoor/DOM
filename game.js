3d89f9e8083dadd9d9a6d081ddae0a0ab2afba38
Abdullah Alsqoor <alsqoorAbdullah2@gmail.com>
2019-07-21 23:53:41 +0300

    exercises #9 created printConsole and printDisplay methods & created map on dom & added charachter

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
 /* TODO implement game logic here */
+
+window.onload = function() {
+    // create map
+    var labyrinth = new Labyrinth();
+    labyrinth.printConsole();
+    labyrinth.printDisplay('map');
+
+    // charachter
+    var hero = document.createElement('div');
+    document.getElementById('map').appendChild(hero);
+
+    // movement
+}
--- labyrinth.class.js
+++ labyrinth.class.js
@@ -6,7 +6,39 @@ function Labyrinth() {
         [1, 0, 0, 0, 1, 0],
         [1, 0, 1, 0, 0, 0],
         [1, 0, 1, 1, 1, 0],
-        [1, 0, 0, 0, 1, 0]];
+        [1, 0, 0, 0, 1, 0]
+    ];
+
+    this.printConsole = function() {
+        this.map.forEach(function(row) {
+            var rowCells = '';
+            row.forEach(function(cell) {
+                if(cell === 0)
+                    rowCells += ' '
+                else 
+                    rowCells += '*'
+            });
+            console.log(rowCells);
+        });
+    };
+
+    this.printDisplay = function(id) {
+        var element = document.getElementById(id);
+        var newDiv;
+        var classList;
+
+        this.map.forEach(function(row) {
+            row.forEach(function(cell) {
+                newDiv = document.createElement('div');
+                classList = 'cell';
+                cell === 0 ? classList += ' road' : classList += ' wall';
+                newDiv.classList = classList;
+                element.appendChild(newDiv);
+            });
+        });
+    };
 }
 
 /* TODO implement core labyrinth functionality here */
+
+
